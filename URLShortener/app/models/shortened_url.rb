7ae7b2429bb2262ef5f70e3e9089ebbc80897a8c
AndrewYW <andrewwang39@gmail.com>
2018-12-12 16:07:22 -0500

    update shortened_url.rb

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -15,9 +15,21 @@ class ShortenedURL < ActiveRecord::Base
   validates :short_url, uniqueness: true
 
   belongs_to :submitter,
-    class_name: :User,
+    class_name: 'User',
     primary_key: :id,
-    foreign_key: user_id
+    foreign_key: :user_id
 
-  
+  def self.make_entry(user, long_url)
+    ShortenedURL.create!(
+      user_id: user.id,
+      long_url: long_url,
+      short_url: ShortenedURL.random_code
+    )
+  end
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+    code = SecureRandom.urlsafe_base64 while ShortenedUrl.exists?(short_url: code)
+    code
+  end
 end
