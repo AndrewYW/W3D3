d4e07fd4b78522b18f23503efc9440b6323802e2
AndrewYW <andrewwang39@gmail.com>
2018-12-12 14:55:54 -0500

    finish model associations

--- rails_skeleton/app/models/course.rb
+++ rails_skeleton/app/models/course.rb
@@ -1,2 +1,20 @@
 class Course < ApplicationRecord
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :course_id,
+    primary_key: :id
+
+  has_many :enrolled_students,
+    through: :enrollments, source: :user
+
+  belongs_to :prerequisite,
+    class_name: 'Course',
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    optional: true
+
+  belongs_to :instructor,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :instructor_id
 end
